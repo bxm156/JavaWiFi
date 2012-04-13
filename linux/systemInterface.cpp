965c1fe87ff2e24197744b5b1ee1f4e88bc655a6
Bryan Marty <bxm156@case.edu>
2012-04-13 00:24:28 -0400

    Added isWiFi() support, and .gitignore

--- linux/systemInterface.cpp
+++ linux/systemInterface.cpp
@@ -46,7 +46,24 @@ JNIEXPORT jboolean JNICALL Java_com_bryanmarty_javawifi_SystemInterface_nativeIn
  */
 JNIEXPORT jboolean JNICALL Java_com_bryanmarty_javawifi_SystemInterface_nativeInterfaceIsWiFi
   (JNIEnv * env, jobject obj, jstring interfaceName) {
-	return 0;
+		jboolean isWiFi = JNI_FALSE;
+
+		const char *iName = env->GetStringUTFChars(interfaceName, NULL);
+		if(iName == NULL) {
+			return isWiFi;
+		}
+
+		std::vector<std::string> interfaces = jw_enumate_interfaces(PROC_NET_WIRELESS);
+		for(unsigned int i = 0; i < interfaces.size(); i++) {
+			const char *name = interfaces[i].c_str();
+			if(strcmp(name,iName) == 0) {
+				isWiFi = JNI_TRUE;
+			}
+		}
+
+		env->ReleaseStringUTFChars(interfaceName,iName);
+
+		return isWiFi;
 }
 
 
--- test/src/com/bryanmarty/javawifi/SystemInterfaceTest.java
+++ test/src/com/bryanmarty/javawifi/SystemInterfaceTest.java
@@ -10,10 +10,17 @@ public class SystemInterfaceTest {
 	public void test() {
 		SystemInterface i1 = new SystemInterface("eth0");
 		assertTrue(i1.exists());
+		assertFalse(i1.isWiFi());
 		SystemInterface i2 = new SystemInterface("lo");
 		assertTrue(i2.exists());
+		assertFalse(i2.isWiFi());
 		SystemInterface i3 = new SystemInterface("bad");
 		assertFalse(i3.exists());
+		SystemInterface i4 = new SystemInterface("ra0");
+		assertTrue(i4.exists());
+		assertTrue(i4.isWiFi());
+		
+		
 	}
 
 }
